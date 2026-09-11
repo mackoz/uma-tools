2ad01a11152450120f9d1457e035ff8afd8a04f9
mackoz <willyumlu@gmail.com>
2026-09-11 02:24:28 -0700

    PIPE-58: drop now-unused skill_data.json type import in HorseDefTypes

--- components/HorseDefTypes.ts
+++ components/HorseDefTypes.ts
@@ -1,6 +1,5 @@
 import { Map as ImmMap, Record } from 'immutable';
 import skillmeta from '../skill_meta.json';
-import type skills from '../uma-skill-tools/data/jp/skill_data.json';
 
 export function isDebuffSkill(id: string) {
 	// iconId 3xxxx is the debuff icons
